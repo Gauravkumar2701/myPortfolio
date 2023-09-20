a0078549fb42aab7be3865b03677912e7318f7ec
Gauravkumar2701 <gouravkumar2701@gmail.com>
2023-09-20 11:51:36 +0530

    removed sytectical error

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,21 +53,21 @@ const about = () => {
             <div className="col-span-3 flex flex-col items-start justify-start">
               <h2 className="mb-4 text-lg font-bold uppercase">Biography</h2>
               <p className="font-medium ">
-                Hi, I'm Gaurav, a web developer and DevOps Engineer with a
+                Hi, I&apos;m Gaurav, a web developer and DevOps Engineer with a
                 passion for creating beautiful, functional, and user-centered
                 digital experiences. With 4 years of experience in the field. I
                 am always looking for new and innovative ways to bring my
-                clients' visions to life.
+                clients&apos; visions to life.
               </p>
 
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty – it&apos;s about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
 
               <p className="font-medium">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I&apos;m working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
